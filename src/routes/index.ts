bdba7d16fe88ebc2ce05f7984f42fdb934ac5520
gabito1451 <obiagaziegabriel@gmail.com>
2026-03-08 03:26:10 -0700

    Implement Authentication Controller & Routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import userRoutes from './v1/users.routes'
+import authRoutes from './v1/auth.routes'
 
-const router = Router()
+const router: Router = Router()
 
 router.get('/', (req, res) => {
   res.json({ message: 'API is running' })
 })
 
 router.use('/v1/users', userRoutes)
+router.use('/v1/auth', authRoutes)
 
 export default router
