e11d7eb56d35da7e694565bd403091aa8bd8f75f
7udah <merlik787@gmail.com>
2026-07-07 14:34:07 +0000

    test(setup): pin DATABASE_URL default to orivex_backend

--- tests/setup.ts
+++ tests/setup.ts
@@ -3,4 +3,4 @@ import { config } from 'dotenv'
 config({ path: '.env.test' })
 
 process.env.DATABASE_URL ??=
-  'postgresql://user:password@localhost:5432/learnault'
+  'postgresql://user:password@localhost:5432/orivex'
